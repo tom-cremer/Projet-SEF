bda21607ca2dfbb443d1210be7dc1098b7833211
tomcremer <tom.cremer@student.hepl.be>
2024-06-17 02:04:04 +0200

    Php Upgrade

--- sef-2024/template-actualite.php
+++ sef-2024/template-actualite.php
@@ -9,52 +9,62 @@
 
 <section class="news-section">
     <div class="news-content" itemscope itemtype="https://schema.org/NewsArticle">
-        <h2 class="news-title"><span>Les News du</span> <span itemprop="name" class="highlight">SEF</span></h2>
-        <p class="news-description" itemprop="description">Ici, vous trouverez tous ce qui concerne les événements que
-            nous organisons</p>
+        <h2 class="news-title"><span><?= get_field('titre'); ?></span> <span itemprop="name"
+                                                                             class="highlight"><?= get_field('highlight'); ?></span>
+        </h2>
+        <p class="news-description" itemprop="description"><?= get_field('description'); ?></p>
     </div>
     <div class="news-image">
-        <img src="https://placeholder.co/520x370" alt="News Image" class="square" itemprop="image">
+        <?php if (get_field('image')): ?>
+            <img src="<?= get_field('image')['url']; ?>" alt="<?= get_field('image')['alt']; ?>" class="square"
+                 itemprop="image">
+        <?php endif; ?>
     </div>
 </section>
 
 <section class="event-section">
-    <h2>Événement à Venir</h2>
+    <h2>News</h2>
     <div class="event-cards" itemscope itemtype="https://schema.org/Event">
-        <article class="event-card">
-            <img src="https://placeholder.co/300x200" alt="Événement à venir" itemprop="image">
-            <div class="event-info">
-                <h3 itemprop="name">Événement à venir</h3>
-                <p class="date" itemprop="date">04/08/2024</p>
-                <p itemprop="description">Grand barbecue de soutien ce samedi 22 juin à 12h pour financer nos projets
-                    communautaires.</p>
-                <a href="#" class="event-link" itemprop="url">Voir l'article</a>
-            </div>
-        </article>
+        <?php if (have_posts()): while (have_posts()): the_post(); // Ouverture de "The Loop" de Wordpress ?>
+            <?php
+            // Créer une nouvelle requête Wordpress pour récupérer mes 3 derniers projets en date.
+
+            $articles = new WP_Query([
+                'post_type' => 'event',
+                'post_status' => 'publish',
+                'posts_per_page' => 7,
+                'orderby' => 'date',
+                'order' => 'DESC',
+            ]);
+            ?>
+
+
+            <?php
+            // Boucler via "The Loop" sur le résultat de cette requête
+            if ($articles->have_posts()): while ($articles->have_posts()): $articles->the_post();
+                ?>
+                <article class="event-card">
+                    <?php if (get_field('image')): ?>
+                        <img src="<?= get_field('image')['url']; ?>" alt="<?= get_field('image')['alt']; ?>"
+                             itemprop="image">
+                    <?php endif; ?>
+                    <div class="event-info">
+                        <h3 itemprop="name"><?= get_field('titre'); ?></h3>
+                        <p class="date" itemprop="date"><?= get_field('date'); ?></p>
+                        <p itemprop="description"><?= get_field('description'); ?></p>
+                        <a href="<?= get_permalink(); ?>" class="event-link" itemprop="url">Voir l'article</a>
+                    </div>
+                </article>
+            <?php endwhile; endif; // Fin de "The Loop" des recettes ?>
+
+        <?php endwhile; endif; // Fin de "The Loop" des recettes ?>
 
     </div>
-    <div class="event-button">
-        <a href="#" class="see-more">Voir Plus</a>
-    </div>
+    <!--    <div class="event-button">
+        <a href="<?php /*= get_field('actualite'); */ ?>" class="see-more">Voir Plus</a>
+    </div>-->
 </section>
 
-<section class="event-section">
-    <h2>Événement Passée</h2>
-    <div class="event-cards" itemscope itemtype="https://schema.org/Event">
-        <article class="event-card">
-            <img src="https://placeholder.co/300x200" alt="Événement passée" itemprop="image">
-            <div class="event-info">
-                <h3 itemprop="name">Événement</h3>
-                <p itemprop="description">Grand barbecue de soutien ce samedi 22 juin à 12h pour financer nos projets
-                    communautaires.</p>
-                <a href="#" class="event-link" itemprop="url">Voir l'article</a>
-            </div>
-        </article>
-    </div>
-    <div class="event-button">
-        <a href="#" class="see-more">Voir Plus</a>
-    </div>
-</section>
 
 
 <?php get_footer(); ?>
